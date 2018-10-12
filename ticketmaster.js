7969b35cc9589b24613c9da8dd5632c72f9d3f77
Tops <mtoplisek95@gmail.com>
2018-10-12 12:02:45 -0400

    got functionality working so that the user can enter a start and end date to filter the results of the events

--- ticketmaster.js
+++ ticketmaster.js
@@ -1,20 +1,26 @@
 const displayEvents = function (e) {
-    e.preventDefault()
+    e.preventDefault();
+    $('#eventDump').empty();
     const APIKey = 'epTJybwaqGagagV0AixUnakzqCbT5q0I';
     const consumerSecret = 'Y4xbmMn0fXD5NzA4';
 
     let eventSearchTerm = $('#eventName').val();
     let eventCity = $('#eventCity').val();
+    let startDate = $('#startDate').val();
+    let endDate = $('#endDate').val();
 
-    let queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${APIKey}&keyword=${eventSearchTerm}&city=${eventCity}&size=5`;
+
+    let queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${APIKey}&startDateTime=${startDate}T00:00:00Z&endDateTime=${endDate}T23:59:59Z&keyword=${eventSearchTerm}&city=${eventCity}&size=5`;
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
         console.log(response);
         for (let i = 0; i < response._embedded.events.length; i++) {
-        $("#eventDump").append(`<div>${response._embedded.events[i].name}</div>`)
-        console.log(response._embedded.events[i].name)
+            $("#eventDump").append(`<div><p>${response._embedded.events[i].name}</p>
+            <p>${response._embedded.events[i].dates.start.localDate}</p>
+            <p>${response._embedded.events[i]._embedded.venues[0].city.name}</p></div>`)
+            console.log(response._embedded.events[i].name)
         }
     })
 }
