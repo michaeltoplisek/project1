b92045082709e10aa40bf5937af8f0ff9ac1f384
Mark J <mjohnson4406@outlook.com>
2018-10-12 14:25:20 -0400

    removed function call at the bottom

--- gibibo.js
+++ gibibo.js
@@ -4,7 +4,7 @@
 
 
 const getFlightInfo = function () {
-  const queryURL="https://developer.goibibo.com/api/search/?app_id=6f6f5b90&app_key=99f325dd41ae1c6f61ce55285d94e449&format=json&source=ATL&destination=ABI&dateofdeparture=20181013&dateofarrival=20181016&seatingclass=E&adults=1&children=0&infants=0&counter=100"
+  const queryURL="https://developer.goibibo.com/api/search/?app_id=6f6f5b90&app_key=99f325dd41ae1c6f61ce55285d94e449&format=json&source=ATL&source=RRY&destination=ABI&dateofdeparture=20181013&dateofarrival=20181016&seatingclass=E&adults=1&children=0&infants=0&counter=100"
   //
   //
   // Query URL with Template Literals "https://developer.goibibo.com/api/search/?app_id=6f6f5b90&app_key=99f325dd41ae1c6f61ce55285d94e449&format=json&source=${iataSourceCode}&destination=${iataDesCode}&dateofdeparture=${depDate}&dateofarrival=${returnDate}&seatingclass=E&adults=1&children=0&infants=0&counter=100"
@@ -23,16 +23,16 @@ const getFlightInfo = function () {
   // hash for flight, Airline Name, Origin airport,Departure time,Overall flight duration,Number of stops, destination, secondary flight destination, arrival time of secondary flight, total cost of trip
   for (let i = 0; i < shortArray.length; i++) {
     $('.flight-details').append(`<div>
-      <p> ${shortArray[i].FlHash} </p>
-      <p> ${shortArray[i].airline} </p>
-      <p> ${shortArray[i].origin} </p>
-      <p> ${shortArray[i].deptime} </p>
-      <p> ${shortArray[i].duration} </p>
-      <p> ${shortArray[i].stops} </p>
-      <p> ${shortArray[i].destination} </p>
-      <p> ${shortArray[i].onwardflights[0].destination} </p>
-      <p> ${shortArray[i].onwardflights[0].arrtime} </p>
-      <p> ${shortArray[i].fare.grossamount} </p></div>`)
+      <p> Flight Code: ${shortArray[i].FlHash} </p>
+      <p> Airline Name: ${shortArray[i].airline} </p>
+      <p> Flying out of: ${shortArray[i].origin} </p>
+      <p> Departure Time: ${shortArray[i].deptime} </p>
+      <p> Flight Duration: ${shortArray[i].duration} </p>
+      <p> Number of Stops: ${shortArray[i].stops} </p>
+      <p> Destination Airport: ${shortArray[i].destination} </p>
+      <p> Connecting Airport: ${shortArray[i].onwardflights[0].destination} </p>
+      <p> Arrival Time: ${shortArray[i].onwardflights[0].arrtime} </p>
+      <p>  Total Cost: ${shortArray[i].fare.grossamount} </p></div>`)
   }
 
   });
