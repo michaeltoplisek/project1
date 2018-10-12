7ead05d70cf49eda3e51055276cc5bc509b720b7
Mark J <mjohnson4406@outlook.com>
2018-10-12 13:38:03 -0400

    added additional result parameters in api call

--- gibibo.js
+++ gibibo.js
@@ -1,13 +1,32 @@
-const queryURL="https://developer.goibibo.com/api/search/?app_id=6f6f5b90&app_key=99f325dd41ae1c6f61ce55285d94e449&format=json&source=ATL&destination=ABI&dateofdeparture=20181031&dateofarrival=20181202&seatingclass=E&adults=1&children=0&infants=0&counter=100"
+const queryURL="https://developer.goibibo.com/api/search/?app_id=6f6f5b90&app_key=99f325dd41ae1c6f61ce55285d94e449&format=json&source=ATL&destination=ABI&dateofdeparture=20181013&dateofarrival=20181016&seatingclass=E&adults=1&children=0&infants=0&counter=100"
 //
-// "https://developer.goibibo.com/api/search/?app_id=6f6f5b90&app_key=${APIKey}&format=json&source=${departureCity}&destination=${destinationCity}&dateofdeparture=${departureDate}&dateofarrival=${arrivalDate}&seatingclass=E&adults=1&children=0&infants=0&counter=100"
-
+//
+// Query URL with Template Literals "https://developer.goibibo.com/api/search/?app_id=6f6f5b90&app_key=99f325dd41ae1c6f61ce55285d94e449&format=json&source=${iataSourceCode}&destination=${iataDesCode}&dateofdeparture=${depDate}&dateofarrival=${returnDate}&seatingclass=E&adults=1&children=0&infants=0&counter=100"
 
 
 $.ajax({
   url: queryURL,
   method: 'GET'
 }).then(function(result) {
-console.log(result);
+
+// shorten the results
+  const shortArray = result.data.onwardflights.slice(0 , 5)
+  console.log(shortArray);
+
+// loop through results to get the required information.
+// hash for flight, Airline Name, Origin airport,Departure time,Overall flight duration,Number of stops, destination, secondary flight destination, arrival time of secondary flight, total cost of trip
+for (let i = 0; i < shortArray.length; i++) {
+  $('.flight-details').append(`<div>
+    <p> ${shortArray[i].FlHash} </p>
+    <p> ${shortArray[i].airline} </p>
+    <p> ${shortArray[i].origin} </p>
+    <p> ${shortArray[i].deptime} </p>
+    <p> ${shortArray[i].duration} </p>
+    <p> ${shortArray[i].stops} </p>
+    <p> ${shortArray[i].destination} </p>
+    <p> ${shortArray[i].onwardflights[0].destination} </p>
+    <p> ${shortArray[i].onwardflights[0].arrtime} </p>
+    <p> ${shortArray[i].fare.grossamount} </p></div>`)
+}
 
 });
