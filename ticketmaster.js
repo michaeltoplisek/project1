ee402ab5dbef6b1a9884600b51f69a5fa3603baf
Tops <mtoplisek95@gmail.com>
2018-10-12 11:07:38 -0400

    Got functionality of on click to display event names for the search

--- ticketmaster.js
+++ ticketmaster.js
@@ -3,18 +3,20 @@ const displayEvents = function (e) {
     const APIKey = 'epTJybwaqGagagV0AixUnakzqCbT5q0I';
     const consumerSecret = 'Y4xbmMn0fXD5NzA4';
 
-    let eventSearchTerm =  $('#eventName').val();
-    let eventCity =   $('#eventCity').val();
+    let eventSearchTerm = $('#eventName').val();
+    let eventCity = $('#eventCity').val();
 
-    let queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${APIKey}&keyword=${eventSearchTerm}&city=${eventCity}`;
+    let queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${APIKey}&keyword=${eventSearchTerm}&city=${eventCity}&size=5`;
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
-            console.log(response);
-            console.log(response._embedded.events[0].name)
-            $("#eventDump").append(response._embedded.events[0].name)
-        })
+        console.log(response);
+        for (let i = 0; i < response._embedded.events.length; i++) {
+        $("#eventDump").append(`<div>${response._embedded.events[i].name}</div>`)
+        console.log(response._embedded.events[i].name)
+        }
+    })
 }
-//displayEvents()
+
 $('#searchButton').on('click', displayEvents)
